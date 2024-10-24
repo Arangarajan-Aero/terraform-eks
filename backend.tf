f9635334df732e5b8ff6d7b4c0fec744bd574c6a
arangarajan <arangarajanmoorthy002@gmail.com>
2024-10-24 15:13:00 +0530

    backend-moified

--- backend.tf
+++ backend.tf
@@ -1,20 +1,9 @@
-resource "aws_dynamodb_table" "dynamodb_terraform_state_lock" {
-  name     = "terraform-state-lock-dynamo"
-  hash_key = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 terraform {
   backend "s3" {
-    bucket         = "backend-statelock-s3-buckert" 
+    bucket         = "tf-jenkins-s3"
     key            = "terraform/eks-cluster.tfstate"
-    region         = var.aws_region
-    dynamodb_table = aws_dynamodb_table.dynamodb_terraform_state_lock.name
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
-
